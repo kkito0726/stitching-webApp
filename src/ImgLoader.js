62d7e69f24d271c6d99f0d5a5797885fcd18e918
kkito0726 <k.kito0726@gmail.com>
2022-08-27 15:22:51 +0900

    map関数でimgタグを生成するようにした。

--- src/ImgLoader.js
+++ src/ImgLoader.js
@@ -2,18 +2,16 @@ import React from 'react'
 import { ImgSender } from './ImgSender'
 
 export const ImgLoader = ({path, image}) => {
-    let list = []
 
-    for (let i in path) {
-        list.push(<img src={path[i]} className="readImg" key={i} name="img" />)
-    }
+    const roadImgList = path.map((value, index) => <img src={value} className="readImg" key={index} name="img" />)
+    
   return (
     <div>
       <div style={{marginTop: "30px"}}>
         <b>
-          {list.length}枚の画像が読み込まれました
+          {roadImgList.length}枚の画像が読み込まれました
         </b>
-      <p>{list}</p>
+        <p>{roadImgList}</p>
       </div>
       
       <form action="" name='img'>
